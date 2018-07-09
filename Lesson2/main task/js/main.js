76cffb5198309be0c80abec4640909c9fe3ae6df
Flash <chessbarryallen@gmail.com>
2018-07-09 13:08:56 +1100

    Loading homework number two and correction mistakes from first homework

--- Lesson2/main task/js/main.js
+++ Lesson2/main task/js/main.js
@@ -16,5 +16,15 @@ for (var i = 0; i < 3; i++) {
     mainList.shopGoods[i] = prompt('Какой тип товаров будем продавать:');
 }
 
-alert(budget / 30);
+// var j = 0;
+// while (j < 3) {
+//     mainList.shopGoods[i] = prompt('Какой тип товаров будем продавать:');
+// }
+//
+// j = 0;
+// do {
+//     mainList.shopGoods[i] = prompt('Какой тип товаров будем продавать:');
+// } while (j < 3);
+
+alert(mainList.budget / 30);
 console.log(mainList.shopGoods);
